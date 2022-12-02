5f5bf7aa9fd370dd97314c5e1944f93f4a7f4b05
Aleksei Samoilov <alekseisamoilov064@gmail.com>
2022-12-02 19:26:12 +0400

    Создание счета и привязка к клиенту

--- src/main/java/com/kluevja/bankappweb/controllers/AccountController.java
+++ src/main/java/com/kluevja/bankappweb/controllers/AccountController.java
@@ -58,7 +58,6 @@ public class AccountController {
 =======
 =======
 import com.kluevja.bankappweb.models.Account;
-import com.kluevja.bankappweb.models.Client;
 import com.kluevja.bankappweb.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 >>>>>>> c630f34 (Add AccountService, AccountController)
@@ -67,7 +66,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@Controller// Controller
 @RequestMapping("/account")
 
 public class AccountController {
@@ -90,13 +89,13 @@ public class AccountController {
     @Autowired
     public AccountService accountService;
     @PostMapping("/create")
-    public ModelAndView create (@ModelAttribute Account account, @RequestParam Long id, RedirectAttributes model) {
-        if (accountService.createAccount(account, id)) {
+    public ModelAndView create (@RequestParam Long id, RedirectAttributes model) {
+        if (accountService.createAccount(id)) {
             model.addFlashAttribute("msg", "Счет успешно создан");
         } else {
             model.addFlashAttribute("msg", "Не удалось создать счет");
         }
-        return  new ModelAndView("redirect:/account");
+        return new ModelAndView("redirect:/account");
     }
     @GetMapping("/get")
     public ModelAndView get (@RequestParam Long id, RedirectAttributes model) {
