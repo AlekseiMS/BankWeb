ae824292343bdc50647fa2ff7016f1359fef9d98
Aleksei Samoilov <alekseisamoilov064@gmail.com>
2022-12-08 11:30:38 +0400

    We continue to work with authorization

--- src/main/java/com/kluevja/bankappweb/configurations/SecurityConfiguration.java
+++ src/main/java/com/kluevja/bankappweb/configurations/SecurityConfiguration.java
@@ -1,15 +1,28 @@
 package com.kluevja.bankappweb.configurations;
 
+<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
+=======
+import net.bytebuddy.implementation.bind.annotation.BindingPriority;
+>>>>>>> 10e576b (We continue to work with authorization)
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+<<<<<<< HEAD
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+=======
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+>>>>>>> 10e576b (We continue to work with authorization)
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -33,11 +46,23 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+<<<<<<< HEAD
         http.cors().and().csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
                         .antMatchers("/", "/login/**", "/css/**", "/js/**").permitAll()
                         .antMatchers("/client", "/account").hasAuthority("OPER")
+=======
+
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/","/login").permitAll()//,"/client/**" попробовать зайти и создать пароль и дб
+>>>>>>> 10e576b (We continue to work with authorization)
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
--- src/main/java/com/kluevja/bankappweb/controllers/ClientController.java
+++ src/main/java/com/kluevja/bankappweb/controllers/ClientController.java
@@ -4,6 +4,7 @@ import com.kluevja.bankappweb.models.Client;
 import com.kluevja.bankappweb.services.ClientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
